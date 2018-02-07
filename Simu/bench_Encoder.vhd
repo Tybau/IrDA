d4566b8a1312cb95da9b4c94e9911a5727e3aa07
vdubois <victor.dubois1@etu.upmc.fr>
2018-02-07 09:49:08 +0100

    modify bench_Encoder

--- Simu/bench_Encoder.vhd
+++ Simu/bench_Encoder.vhd
@@ -10,27 +10,30 @@ architecture arch_bench_encoder of bench_encoder is
   signal Cmd : std_logic_vector(5 downto 0);
   signal Go : std_logic;
   signal Tx : std_logic;
+  
+BEGIN
 
+  encoder : entity work.Encoder(behavioral)
+    port map(CLK => CLK, Address => Address, Cmd => Cmd, Go => Go, Tx => Tx);
+  
+  cloked : process
   BEGIN
+    CLK <= '0';
+    wait for 10 ns;
+    CLK <= '1';
+    wait for 10 ns;
+  end process cloked;
+  
+  Test : process
+  BEGIN
+    Address <= "11010";
+    Cmd <= "101100";
+    wait for 100 ns;
+    Go <= '1';
+    wait for 300 ms;
+    Go <= '0';
+    wait for 100 ms;
+  end process Test;
+  
+end architecture arch_bench_encoder;
 
-    cloked : process
-      BEGIN
-        CLK <= '0';
-        wait for 10 ns;
-        CLK <= '1';
-        wait for 10 ns;
-      end process cloked;
-      
-      Test : process
-        BEGIN
-          Address <= "11010";
-          Cmd <= "101100";
-          wait for 100 ns;
-          Go <= '1';
-          wait for 300 ms;
-          Go <= '0';
-          wait for 100 ms;
-        end process Test;
-
-      end architecture arch_bench_encoder;
-        
